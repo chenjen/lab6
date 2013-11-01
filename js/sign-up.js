1cb99557dc793d778becf651838581d5a46d750c
Khang Tran <KhangTran@Khangs-MacBook-Pro-2.local>
2013-10-31 21:42:10 -0700

    final

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,41 @@
 //signup.js 
 // add your JavaScript code to this file
 
+//function that will be called when the 
+//document is ready for manipulation
+$(function(){
+    //document is now ready for manipulation
+    var stateSelect = $('select[name="state"]');
+    var idx;
+    var state;
+    for (idx = 0; idx < usStates.length; ++idx) {
+        state = usStates[idx];
+        option = $(document.createElement('option'));
+        option.attr('value', state.abbreviation);
+        option.html(state.name);
+        stateSelect.append(option);   
+}
+
+    $('.signup-form').submit(function(){
+        var addr1Input = $('.signup-form input[name="addr-1"]');
+        var addr1Value = addr1Input.val();
+        var zipInput = $('.signup-form input[name="zip"]');
+        var zipValue = zipInput.val();
+        if (addr1Value && addr1Value.trim().length && zipValue && zipValue.trim().length > 0) { 
+            return true;
+        }
+        else if (addr1Value == 0) {
+            return true;
+        }
+        else {
+            alert("Please enter a valid address.");
+            return false;                        
+        }
+    });
+
+
+    $('.cancel-signup').click(function(){
+        //code to run when user clicks "No Thanks!" button
+        window.location = 'http://www.google.com';
+    }); //cancel-signup click
+}); //on document ready 
